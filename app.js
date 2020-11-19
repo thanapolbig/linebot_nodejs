4c30074d4dc88ecd6cac02fbc514619c620699fa
Thanapol <thanabig000@gmail.com>
2020-11-19 20:41:01 +0700

    uid

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-// Echo reply
-
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
@@ -10,21 +8,28 @@ app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
     var msg = req.body.events[0].message.text
-    var id = req.body.destination
-    reply(reply_token,id, msg )
+    var id = req.body.events[0].source.userId //get uid from line
+    console.log(id)
+    login(reply_token,id,msg) //verify uid
     res.sendStatus(200)
 })
-app.listen(port)
-function reply(reply_token, id , msg) {
+app.listen(port)    
+function login(reply_token,id,msg){ 
+    if (id === "Ua989cf7b82e0e7fd5e6259ff7633f6c9"){    //check db
+        massage = "check in success"
+        console.log(massage)
+        reply(reply_token, msg )
+    }else{
+        msg = "uid not found"
+        reply(reply_token, msg )
+    }
+}
+
+function reply(reply_token, msg) {
     let headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer {shWHPy2AtysDJ8IkWowJC/NTQgpxu5ddDJ3GwD3eUQ6FcDnZ+Q1VgBrF9xyusFwet9PK5cBPmmxxLthorhuZrL94biE5JuQ+aJjEboIP5dQDLsgXMur0+QiioiMCL+xJxlA3aG5aFRDGy2HhHEPpfgdB04t89/1O/w1cDnyilFU=}'
-    }
-
-    if (id === "Ud25f8d3070e87673ed0f8e33e0ea6390"){
-        msg = "check in success"
-        console.log(msg)
-    }
+    }    
     if (msg === "Big") {
         msg = "หัวควย"
         console.log(msg)
